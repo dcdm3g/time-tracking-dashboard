d690ee0107187851a9c601dd9379c40ea22471ff
Davi Melo <davicosta01082008@gmail.com>
2024-06-27 13:41:43 -0300

    refactor: refactor tailwind content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['src/components/*.tsx', 'src/app/*.tsx'],
+  content: ['src/components/*.tsx', 'src/app/layout.tsx'],
   theme: {
     colors: {
       white: '#FFFFFF',
